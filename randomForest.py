f317fd233c5a50f8ebf7cdfe533ea091e50b0d26
Kenny H <nobi3892@gmail.com>
2016-10-18 09:31:23 +0900

    in class

--- randomForest.py
+++ randomForest.py
@@ -1,12 +1,105 @@
-# Import the random forest package
-from sklearn.ensemble import RandomForestClassifier 
+""" Writing my first randomforest code.
+Author : AstroDave
+Date : 23rd September 2012
+Revised: 15 April 2014
+please see packages.python.org/milk/randomforests.html for more
 
-# Create the random forest object which will include all the parameters
-# for the fit
-forest = RandomForestClassifier(n_estimators = 100)
+""" 
+import pandas as pd
+import numpy as np
+import csv as csv
+from sklearn.ensemble import RandomForestClassifier
+import pylab as P
 
-# Fit the training data to the Survived labels and create the decision trees
-forest = forest.fit(train_data[0::,1::],train_data[0::,0])
 
-# Take the same decision trees and run it on the test data
-output = forest.predict(test_data)
+# Data cleanup
+# TRAIN DATA
+train_df = pd.read_csv('csv/train.csv', header=0)        # Load the train file into a dataframe
+
+# I need to convert all strings to integer classifiers.
+# I need to fill in the missing values of the data and make it complete.
+
+# female = 0, Male = 1
+train_df['Gender'] = train_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+
+# Embarked from 'C', 'Q', 'S'
+# Note this is not ideal: in translating categories to numbers, Port "2" is not 2 times greater than Port "1", etc.
+
+# All missing Embarked -> just make them embark from most common place
+if len(train_df.Embarked[ train_df.Embarked.isnull() ]) > 0:
+    train_df.Embarked[ train_df.Embarked.isnull() ] = train_df.Embarked.dropna().mode().values
+
+Ports = list(enumerate(np.unique(train_df['Embarked'])))    # determine all values of Embarked,
+Ports_dict = { name : i for i, name in Ports }              # set up a dictionary in the form  Ports : index
+train_df.Embarked = train_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)     # Convert all Embark strings to int
+
+# All the ages with no data -> make the median of all Ages
+median_age = train_df['Age'].dropna().median()
+if len(train_df.Age[ train_df.Age.isnull() ]) > 0:
+    train_df.loc[ (train_df.Age.isnull()), 'Age'] = median_age
+
+# Remove the Name column, Cabin, Ticket, and Sex (since I copied and filled it to Gender)
+train_df = train_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1) 
+
+
+# TEST DATA
+test_df = pd.read_csv('csv/test.csv', header=0)        # Load the test file into a dataframe
+
+# I need to do the same with the test data now, so that the columns are the same as the training data
+# I need to convert all strings to integer classifiers:
+# female = 0, Male = 1
+test_df['Gender'] = test_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+
+# Embarked from 'C', 'Q', 'S'
+# All missing Embarked -> just make them embark from most common place
+if len(test_df.Embarked[ test_df.Embarked.isnull() ]) > 0:
+    test_df.Embarked[ test_df.Embarked.isnull() ] = test_df.Embarked.dropna().mode().values
+# Again convert all Embarked strings to int
+test_df.Embarked = test_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)
+
+
+# All the ages with no data -> make the median of all Ages
+median_age = test_df['Age'].dropna().median()
+if len(test_df.Age[ test_df.Age.isnull() ]) > 0:
+    test_df.loc[ (test_df.Age.isnull()), 'Age'] = median_age
+
+# All the missing Fares -> assume median of their respective class
+if len(test_df.Fare[ test_df.Fare.isnull() ]) > 0:
+    median_fare = np.zeros(3)
+    for f in range(0,3):                                              # loop 0 to 2
+        median_fare[f] = test_df[ test_df.Pclass == f+1 ]['Fare'].dropna().median()
+    for f in range(0,3):                                              # loop 0 to 2
+        test_df.loc[ (test_df.Fare.isnull()) & (test_df.Pclass == f+1 ), 'Fare'] = median_fare[f]
+
+# Collect the test data's PassengerIds before dropping it
+ids = test_df['PassengerId'].values
+# Remove the Name column, Cabin, Ticket, and Sex (since I copied and filled it to Gender)
+test_df = test_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1) 
+
+
+# The data is now ready to go. So lets fit to the train, then predict to the test!
+# Convert back to a numpy array
+
+train_data = train_df.values
+test_data = test_df.values
+
+print train_df.head()
+print test_df.head()
+
+
+print 'Training...'
+forest = RandomForestClassifier(n_estimators=100)
+forest = forest.fit( train_data[0::,1::], train_data[0::,0] )
+# first parameter taking all value in train_data except 0 column
+#second parameter taking all survivor column. 
+print 'Predicting...'
+output = forest.predict(test_data).astype(int)
+#so except survivor column, it has to match with train_data.
+
+
+predictions_file = open("csv/myfirstforest.csv", "wb")
+open_file_object = csv.writer(predictions_file)
+open_file_object.writerow(["PassengerId","Survived"])
+open_file_object.writerows(zip(ids, output))
+predictions_file.close()
+print 'Done.'
--- titanicPanda.py
+++ titanicPanda.py
@@ -3,6 +3,15 @@ import numpy as np
 import pandas as pd
 import pylab as P
 
+# Import the random forest package
+from sklearn.ensemble import RandomForestClassifier 
+
+
+
+
+
+
+
 # csv_file_object = csv.reader(open('csv/train.csv', 'rb')) 
 # header = csv_file_object.next() 
 # data=[] 
@@ -16,6 +25,7 @@ import pylab as P
 # print ages_onboard
 
 df = pd.read_csv('csv/train.csv', header=0)
+data = df
 #print df
 
 #this is for datatypes
@@ -48,8 +58,122 @@ df = pd.read_csv('csv/train.csv', header=0)
 
 
 
-df['Age'].dropna().hist(bins=16, range=(0,80), alpha = .5)
-P.show()
+#df['Age'].dropna().hist(bins=16, range=(0,80), alpha = .5)
+#P.show()
+
+
+
+
+
+
+
+
+###########Clearning the data
+
+
+df['Gender'] = 4
+df['Gender'] = df['Sex'].map( lambda x: x[0].upper() )
+#Iambda x is an built-in function of python for generating 
+#an anonymous function in the moment, at runtime.
+# Remember that x[0] of any string returns its first character.
+df['Gender'] = df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+#changing female = 0 male = 1
+
+#print df['Gender']
+
+
+
+
+
+
+
+#now create 2d array. row = gender column = Pclass
+
+median_ages = np.zeros((2,3))
+#print median_ages
+#zero array
+
+for i in range(0,2):
+	for j in range(0,3):
+		median_ages[i,j] = df[(df['Gender'] == i) & \
+                              (df['Pclass'] == j+1)]['Age'].dropna().median()
+#we get median of ages for each gender and pclass.
+
+#print median_ages
+
+
+df['AgeFill'] = df['Age']
+
+df.head() # head in default displays 5
+
+#so it's displaying each where Age = null. AgeFill is added as same as Age
+#print df[ df['Age'].isnull() ][['Gender','Pclass','Age','AgeFill']].head(10)
+
+
+
+
+#now put null to Age, and filling AgeFill
+for i in range(0, 2):
+    for j in range(0, 3):
+        df.loc[ (df.Age.isnull()) & (df.Gender == i) & (df.Pclass == j+1),\
+                'AgeFill'] = median_ages[i,j]
+
+
+#tara--
+#print df[ df['Age'].isnull() ][['Gender','Pclass','Age','AgeFill']].head(10)
+
+
+#this is a checker wether (Age=Null) = (Age= nonNull)
+df['AgeIsNull'] = pd.isnull(df.Age).astype(int)
+
+
+#parch = number of parents or children
+#SibSp = number of siblings or spouse
+df['FamilySize'] = df['SibSp'] + df['Parch']
+df['Age*Class'] = df.AgeFill * df.Pclass
+
+df['FamilySize'].hist()
+df['Age*Class'].hist()
+#P.show()
+
+
+
+
+
+
+
+
+
+
+#final preparation
+
+
+
+#print df.dtypes
+
+#print df.dtypes[df.dtypes.map(lambda x: x=='object')]
+
+df = df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1) 
+df = df.drop(['Age'], axis=1)
+#print df
+
+
+# df = df.dropna()
+#this one drop any rows which still have missing values
+
+# we just have coverted into numPy array
+train_data = df.values
+
+
+#compare these two!!
+print (train_data)
+print (data)
+
+
+
+
+
+
 
 
 
